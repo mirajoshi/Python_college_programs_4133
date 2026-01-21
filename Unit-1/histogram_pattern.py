345e2111f1b19b9039edab66a397cc0f0c724b27
Mira Joshi <mirajoshi@Miras-MacBook-Air.local>
2026-01-21 15:36:30 +0530

    Histogram code

--- Unit-1/histogram_pattern.py
+++ Unit-1/histogram_pattern.py
@@ -1,3 +1,8 @@
+#Define a procedure histogram() that takes a list of integers and prints a histogram to the screen. For example, histogram([4, 9, 7]) should print the following:
+#****
+#*********
+#*******
+
 def histogram(numbers):
     for i in numbers:
         print("*" * i)
