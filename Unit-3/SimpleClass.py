1fc775dff34580b1cba4f30def9faf4f18e72d3a
Mira Joshi <mirajoshi@Miras-MacBook-Air.local>
2026-03-20 11:27:58 +0530

    Added a code illustrating a simple class in python

---
+++ Unit-3/SimpleClass.py
@@ -0,0 +1,13 @@
+class MyClass:
+    def greet_user(self):
+        print("Hello! Welcome to Python Programming")
+
+    def add_numbers(self):
+        a = 10
+        b = 20
+        sum = a + b
+        print(f"Sum of {a} + {b} = {sum}")
+
+obj = MyClass()
+obj.greet_user()
+obj.add_numbers()
